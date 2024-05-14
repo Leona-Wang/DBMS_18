d84ba00fd2c2ce4eab9cf55aaafd86bc5f0abf93
Leona-Wang <leonawang920913@gmail.com>
2024-05-15 00:03:48 +0800

    2405150003

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -2,19 +2,28 @@ package com.example.demo.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 @Controller
 public class IndexController {
     
     @RequestMapping(value = "/")
-    public String index() {
+    public String login(@RequestParam(name = "username", required = true) String username, @RequestParam(name = "username", required = true) String password, Model model) {
+        // 在這裡可以加入驗證邏輯
+        model.addAttribute("username", username);
+        System.out.println("aaa"+username);
+        return "good";
+    }
+    /*public String index() {
         // 你可以在這裡添加任何需要的邏輯
         System.out.println("index");
         return "index";
-    }
+    }*/
     /*@PostMapping
     public ResponseEntity<String> handleLogin(@RequestParam String username, @RequestParam String password) {
         System.out.println("Username: " + username);
