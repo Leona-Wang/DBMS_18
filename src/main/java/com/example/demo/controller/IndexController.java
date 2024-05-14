788e63bd60cff7e48e2f5199c2075b2a7b1851db
Leona-Wang <leonawang920913@gmail.com>
2024-05-14 22:14:48 +0800

    2405142214

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,18 +1,25 @@
 package com.example.demo.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@RequestMapping("")
+@Controller
 public class IndexController {
     
-    @PostMapping
+    @RequestMapping(value = "/")
+    public String index() {
+        // 你可以在這裡添加任何需要的邏輯
+        System.out.println("index");
+        return "index";
+    }
+    /*@PostMapping
     public ResponseEntity<String> handleLogin(@RequestParam String username, @RequestParam String password) {
         System.out.println("Username: " + username);
         System.out.println("Password: " + password);
         // 这里添加登录逻辑
         return ResponseEntity.ok("Login successful");
-    }
+    }*/
 }
